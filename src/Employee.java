b9c29a00945dfa8d9dad52d79c36a116de26b678
Illytax <61468980+Illytax@users.noreply.github.com>
2020-03-18 18:25:02 +0000

    Seperate classes used for each person

---
+++ src/Employee.java
@@ -0,0 +1,9 @@
+public class Employee extends Person
+{
+	public Employee()
+	{
+		accessLevel = new int[] {1, 2, 3, 4, 5, 6, 7};
+		capacityNeeded = 4;
+		currentFloor = 1;
+	}
+}
--- src/Person.java
+++ src/Person.java
@@ -1,83 +1,18 @@
 public class Person 
 {
-	/*
-	 * String identifier;
-	 */
-	//int[] accessLevel = {1, 2, 3, 4, 5, 6, 7};
-	int priorityLevel;
-	int startingFloor = 0; 
-	int capacityNeeded;
-	int currentFloor = 0; 
-	float probabilityP;
-	float probabilityQ;
+	protected int[] accessLevel;
+	protected int priorityLevel;
+	protected int startingFloor; 
+	protected int capacityNeeded;
+	protected int currentFloor; 
+	protected float probabilityP;
+	protected float probabilityQ;
+	protected int maxWaitTime;
+	protected int currentWaitTime;
+	protected String company;
 	
 	public Person()
 	{
 		
-	}
-	
-	public class Employee
-	{
-		public Employee() 
-		{	
-		
-		}
-	}
-	
-	
-	public class Developer
-	{
-		public Developer() 
-		{
-				
-		}
-	}
-	
-	public class Maintenance 
-	{
-		int capacityNeeded = 4;
-		int spawnProbability;
-		
-		public Maintenance()
-		{
-			
-		}
-	}
-	
-	public class Client
-	{
-		float probabilityQ;
-		String company;
-		int maxWaitTime;
-		int currentWaitTime;
-				
-		public Client() 
-		{
-			
-		}
-		
-		public void fileComplaint() 
-		{
-			System.out.println("File Complaint filed");
-		}
-	}
-	
-	public void setProbabilities(float probP, float probQ) 
-	{
-		probabilityP = probP;
-		probabilityQ = probQ;
-	}
-	
-	public float getProbP()
-	{
-		return probabilityP;
-	}
-	
-	public void goToFloor(int floor)
-	{
-		/* 
-		 * Insert elevator methods here to go up floor
-		 */
-		currentFloor = floor;
 	}
 }
